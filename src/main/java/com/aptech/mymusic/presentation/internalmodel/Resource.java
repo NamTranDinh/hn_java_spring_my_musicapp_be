1499ce20be289457f706c9c09e90fc93197ef8c3
SonNguyen <son15052002@gmail.com>
2023-04-02 10:20:19 +0700

    update url exp to 7Day

--- src/main/java/com/aptech/mymusic/presentation/internalmodel/Resource.java
+++ src/main/java/com/aptech/mymusic/presentation/internalmodel/Resource.java
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Resource {
 
-    public static final long URL_EXP = TimeUnit.DAYS.toMillis(30);
+    public static final long URL_EXP = TimeUnit.DAYS.toMillis(7) - 60 * 1000; // (max 7Day)
 
     public static String getUrl(Path path, String name) {
         return getUrl(ResourceConfig.SYSTEM_RESOURCE_TYPE, path, name);
